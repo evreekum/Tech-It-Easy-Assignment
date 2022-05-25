d67fe18e9f8d1bb6feefc5b05d6baa9c6a4a2908
evreekum <e.v.reekum@gmail.com>
2022-05-25 17:34:37 +0200

    working on opdracht 4

--- functions/mainOpdracht4.js
+++ functions/mainOpdracht4.js
@@ -10,30 +10,85 @@ import inventory from "../data/inventory.js";
 export const createBrandTypeNameFunctie = inventory.map((tvBrandTypeName) => {
     return `${tvBrandTypeName.brand} ${tvBrandTypeName.type} - ${tvBrandTypeName.name}`;
 });
-console.log("Tv: Brand, Type and Name:");
-console.log(createBrandTypeNameFunctie[1]);
+
 
 //FUNCTIE:
-export function tvFormat (selectedTv) {
+export function tvFormat(selectedTv) {
     return `${selectedTv.brand} ${selectedTv.type} - ${selectedTv.name}`;
-};
+}
 
-console.log(tvFormat(inventory[3]))
 
 
 //__________4b______________
 
-export const createTvPrice = inventory.filter((tvPrice) => {
-    return `€${tvPrice === tvPrice.price},-`;
+/*export const createTvPrice = inventory.filter((tvPrice) => {
+    return `€${tvPrice.price === tvPrice},-`;
 });
-console.log(createTvPrice(179));
+const priceInEuro = createTvPrice (179);
+console.log(priceInEuro);*/
 
-/*
-export function pricesTv (tvPrice) {
-    return `€${inventory.filter(tvPrice.price)},-`;
+const currentTvPrice = inventory[0].price;
+
+export function pricesTv(tvPrice) {
+    return `€${tvPrice}-,`;
 }
-const euroPrice = pricesTv(379);
-console.log(euroPrice);*/
+
+export const euroPrice = pricesTv(currentTvPrice);
+
 
 
 //__________4c__________
+
+//inch to cm: 1 inch = 2,54 cm dus: inches / 2,54 = cm
+//map methode?
+//return: grootte inch (grootte cm) | inch (cm)
+//join() (array methode)?
+//for loop
+
+
+export const opdracht4c = ( tv ) => {
+    let output = " ";
+
+    tv.availableSizes.map((size) => {
+        let sizesInCm = Math.round(size / 2.54);
+        output += `${size} inch (${sizesInCm} cm) | `
+    })
+    return output
+
+}
+
+
+
+
+/*
+for (let i = 0; i < availableSizes.length; i++) {
+    return sizeInches.availableSizes / 2.54;
+}
+
+
+const createInchesToCm = () => {
+    return inventory.filter((sizeInches) => {
+
+
+    })
+}
+*/
+
+/*
+const createInchesToCm = inventory.filter((sizeInches) => {
+    for (let i = 0; i < availableSizes.length; i++) {
+        return sizeInches.availableSizes / 2.54;
+    }
+
+})
+console.log(createInchesToCm)
+*/
+
+/*
+export function inchesToCm (tvInch, tvCm) {
+    tvCm = 2.54;
+    return tvInch.availableSizes / tvCm;
+}
+
+console.log(inchesToCm())
+*/
--- main.js
+++ main.js
@@ -1,14 +1,80 @@
 console.log("It is running!")
 // Import variables
-/*import inventory from "./data/inventory.js";*/
+import inventory from "./data/inventory.js";
+import {euroPrice} from "./functions/mainOpdracht4.js";
+
 
 // Import functions
-/*import createTvArrays from "./functions/createTvArrays.js"*/
-/*import mainOpdracht4 from "./functions/mainOpdracht4.js"*/
+//___Opdracht 1___
 import {tvTypes} from "./functions/createTvArrays.js";
 import {soldOuts} from "./functions/createTvArrays.js";
 import {ambiLightTvs, lowToHigh} from "./functions/createTvArrays.js";
+import lowToHightPrice from "./functions/createTvArrays.js";
+
+//___Opdracht 2___
+
+
+//___Opdracht 3___
+
+//___Opdracht 4___
 // import {workIt} from "./functions/mainOpdracht4.js";
 import {createBrandTypeNameFunctie} from "./functions/mainOpdracht4.js";
-// import {tvFormat} from "./functions/mainOpdracht4.js";
-import lowToHightPrice from "./functions/createTvArrays.js";
+import {pricesTv} from "./functions/mainOpdracht4.js";
+import {opdracht4c} from "./functions/mainOpdracht4.js";
+
+import {tvFormat} from "./functions/mainOpdracht4.js";
+
+// import {createTvPrice} from "./functions/mainOpdracht4.js";
+// import {inchesToCm} from "./functions/mainOpdracht4.js";
+
+
+
+
+
+
+//___Inventory___
+/*console.log(inventory);*/
+
+
+//___Opdracht 1___
+
+//____1a___
+console.log("Types of Tv's:")
+console.log(tvTypes);
+
+//___1b___
+console.log("Sold Out Tv's:")
+console.log(soldOuts);
+
+//___1c___
+console.log("Tv's with ambilight:")
+console.log(ambiLightTvs);
+
+//___1d___
+console.log("Tv prices low to high::")
+console.log(lowToHigh);
+
+//___OF___:
+console.log("Tv prices low to high:");
+console.log(lowToHightPrice());
+
+
+//___Opdracht 4___
+
+//___4a___
+console.log("Tv: Brand, Type and Name:");
+console.log(createBrandTypeNameFunctie[1]);
+
+//OF:___
+console.log(tvFormat(inventory[3]));
+
+//___4b___
+console.log("Tv price: ")
+console.log(euroPrice);
+
+//___4c___
+console.log("Tv sizes:")
+console.log(opdracht4c(inventory[0]))
+
+//___4d___
+
